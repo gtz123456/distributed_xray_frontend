335d642dea89bcd09e66ce6d1b7c63c5388fb957
gtz <gtzafw@gmail.com>
2024-10-21 16:14:45 -0700

    migrate from pnpm to npm for github actions

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,7 +22,7 @@ import { it } from "node:test";
 
 export const Navbar = () => {
   return (
-    <NextUINavbar maxWidth="xl" position="sticky" className="overflow-hidden">
+    <NextUINavbar className="overflow-hidden" maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
