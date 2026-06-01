7836f08fab2bf68e2be1810be78a259ef5474537
Tianzhou Gao <gao@Tianzhous-MacBook-Air.local>
2026-05-31 18:03:16 -0700

    fix(ui): remove explicit favicon.ico metadata to fix 500 error

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -17,9 +17,6 @@ export const metadata: Metadata = {
     template: `%s - ${siteConfig.name}`,
   },
   description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
 
 export const viewport: Viewport = {
